8bf4db0085d697c07632d877698a96a8f97ff06d
JINUSHAJI1 <115783882+JINUSHAJI1@users.noreply.github.com>
2022-11-13 23:11:42 +0530

    pattern printing

---
+++ pattern2.js
@@ -0,0 +1,17 @@
+
+  var output = '';
+
+    for (var i = 1; i < 6; i++) {
+
+        for (var j = 1; j <= i; j++) {
+
+            output += j + ' ';
+
+        }
+
+        console.log(output);
+
+        output = '';
+
+    }
+
